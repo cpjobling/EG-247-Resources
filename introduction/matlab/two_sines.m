e17ab6cb68c41ee23a7db8653a499a919ec3d9ea
Chris Jobling <C.P.Jobling@Swansea.ac.uk>
2014-01-17 12:38:28 +0000

    Add two sines example

---
+++ introduction/matlab/two_sines.m
@@ -0,0 +1,16 @@
+%%... plot two sinusoids signal
+A1 = 2; f1 = 1;
+A2 = 1.5;f2 = 2.2;
+t = linspace(0, 3*(1/f1), 1000);
+s1 = A1*cos(2*pi*f1*t);
+s2 = A2*cos(2*pi*f2*t);
+subplot(4,1,1)
+plot(t,s1),ylim([-5,5]),xlabel('Time t (s) '),ylabel('s1')
+subplot(4,1,2)
+plot(t,s2),ylim([-5,5]),xlabel('Time t (s) '),ylabel('s2')
+subplot(4,1,3)
+plot(t,s1+s2),ylim([-5,5]),xlabel('Time t (s) '),ylabel('s2 + s2')
+subplot(4,1,4)
+arrow([f1,0],[f1,A1]),text(f1,A1+0.2,'A1'),text(f1+0.05,0.2,'f1')
+arrow([f2,0],[f2,A2]),text(f2,A2+0.2,'A2'),text(f2+0.05,0.2,'f2')
+axis([0,2.5,0,2.5]),ylabel('Frequency Spectrum'),xlabel('Frequency (Hz)')
