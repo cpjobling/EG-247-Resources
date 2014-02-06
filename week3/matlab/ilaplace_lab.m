eae923920e0ef94e8ba7200e4afb87c466830cf5
Chris Jobling <C.P.Jobling@Swansea.ac.uk>
2014-02-06 10:21:01 +0000

    Add week 3 lab files

---
+++ week3/matlab/ilaplace_lab.m
@@ -0,0 +1,6 @@
+%% Using MATLAB to Solve Inverse Laplace Transform Problems
+%
+% Inspired by Steven Karris, Signals and Systems: With MATLAB Computing and Simulink 
+% Modelling, 5th Edition, Orchard Publications, 2012. 
+% Ebook: <http://site.ebrary.com/lib/swansea/docDetail.action?docID=10547416 Karris, Signals and Systems>
+%
