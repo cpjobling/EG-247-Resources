27d5b6a349b63188dcc07186f168a8a604268099
Chris Jobling <C.P.Jobling@Swansea.ac.uk>
2014-02-23 11:15:44 +0000

    Fourier Part 2 Ready

--- week5/matlab/main.m
+++ week5/matlab/main.m
@@ -22,15 +22,3 @@ for k=1:k_max
     signal = signal + ai(k)*cos(w(k)*t) + bi(k)*sin(w(k)*t)
 end
 plot(t,signal)
- 
-% plot the results from Matlab calculation 
-%stem(w,abs(Xw), 'o-'); 
-%hold on; 
- 
-% the results based on manual calculation (slides # 16) 
-%Xw_manual = tau/T0*sinc(k_vec*tau/T0); 
-%stem(w, abs(Xw_manual), 'r--'); 
- 
-%legend('Matlab calculation', 'Manual calculation'); 
-%xlabel('\omega (rad/sec)'); 
-%ylabel('|c_k|'); 
