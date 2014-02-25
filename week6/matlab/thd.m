2c3b644733cf78ce23886b9528b2e7a0eae5b7b7
Chris Jobling <C.P.Jobling@Swansea.ac.uk>
2014-02-25 15:46:05 +0000

    Add Power Spectrum and THD pictures

---
+++ week6/matlab/thd.m
@@ -0,0 +1,28 @@
+%% Example to illustrate THD
+
+t = linspace(0,4*pi,1000);
+f = zeros(size(t));
+clear C_k
+
+%% Coefficients of sine terms
+bk = [1,0.4,0.1,0.05,0.01];
+
+for k = 1:length(bk)
+    f = f + bk(k).*sin(k*t);
+end
+%%
+plot(t,f)
+title('Distorted sine wave')
+%% Compute power spectrum
+figure
+for k = 1:length(bk)
+    C_k(k) = j.*bk(k)./2
+end
+C_k = [conj(flipdim(C_k,2)),0,C_k];
+%%
+stem([-k:k],abs(C_k).^2)
+axis([-5,5,0.,0.3])
+title('Power Spectrum for Distorted Sine Wave')
+ylabel('Frequency \omega [rad/s]')
+ylabel('|C_k|^2')
+hold off
