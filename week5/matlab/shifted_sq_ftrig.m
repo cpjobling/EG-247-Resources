2e2fbbae77fe58692a48c75ce713dd8624546040
Chris Jobling <C.P.Jobling@Swansea.ac.uk>
2014-02-26 12:51:06 +0000

    Changes to fs scripts

--- week5/matlab/shifted_sq_ftrig.m
+++ week5/matlab/shifted_sq_ftrig.m
@@ -4,28 +4,26 @@
 %
 % Prepared for EG-247 Signals and Systems
 % by Chris Jobling
+%% Initialize problem
 syms t n A pi
-
+%% Define harmonics
 n = [1:11];
 %% DC component
 half_a0 = 0
 %% Compute harmonics - use half-wave symmetry
 ai = 4/pi*int(A*cos(n*t),t,0,(pi/2))
 bi = zeros(size(n))
-%%
-% Reconstruct f(t) from harmonic sine functions
+%% Reconstruct f(t) from harmonic sine functions
 ft = half_a0;
 for k=1:length(n)
-    ft = ft + ai(k)*cos(k*t) + bi(k)*sin(k*t)
+    ft = ft + ai(k)*cos(k*t) + bi(k)*sin(k*t);
 end
 ft
 %% Make numeric
-ft_num = subs(ft,A,1.0)
-%%
+ft_num = subs(ft,A,1.0);
 % print using 4 sig digits
 ft_num = vpa(ft_num, 4)
-%%
-% plot result
+%% plot result
 ezplot(ft_num)
 hold on
 %% plot original signal
