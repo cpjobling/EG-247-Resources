2491f49c64efb9baa5a2a3e787b10325b544aeba
Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2014-03-25 13:01:59 +0000

    create audio demo file

---
+++ week9/matlab/sampling_demo.m
@@ -0,0 +1,14 @@
+% Record your voice for 5 seconds.
+recObj = audiorecorder;
+disp('Start speaking.')
+recordblocking(recObj, 5);
+disp('End of Recording.');
+
+% Play back the recording.
+play(recObj);
+
+% Store data in double-precision array.
+myRecording = getaudiodata(recObj);
+
+% Plot the waveform.
+plot(myRecording);
