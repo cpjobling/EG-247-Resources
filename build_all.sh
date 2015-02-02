40f787f0da641c18592540a209907389d58d55b7
Chris Jobling <cpjobling@gmail.com>
2015-02-02 09:42:55 +0000

    Add build scripts

---
+++ build_all.sh
@@ -0,0 +1,2 @@
+(cd introduction; ./build.sh)
+(cd week1; ./build.sh)
---
+++ introduction/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOC=Introduction
+
+echo Building $DOC.slides.pdf
+pandoc --latex-engine=xelatex -t beamer $DOC.md -o $DOC.slides.pdf
+echo Building $DOC.pdf
+pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
+echo Building $DOC.html
+pandoc --mathjax -s $DOC.md -o $DOC.html
+echo Done
+
+
+
