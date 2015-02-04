5c22bcaefb66e76f6c4f5f5f0d5a3abc7067bdc3
Chris Jobling <cpjobling@gmail.com>
2015-02-04 11:20:57 +0000

    Week 2 resources ready

--- week1/build.sh
+++ week1/build.sh
@@ -3,7 +3,7 @@
 DOC=elementary_signals
 
 echo Building $DOC.slides.pdf
-pandoc --latex-engine=xelatex -t beamer $DOC.md -o $DOC.slides.pdf
+pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -o $DOC.slides.pdf
 echo Building $DOC.pdf
 pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
 echo Building $DOC.html
---
+++ week2/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOC=laplace
+
+echo Building $DOC.slides.pdf
+pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -o $DOC.slides.pdf
+echo Building $DOC.pdf
+pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
+echo Building $DOC.html
+pandoc --mathjax -s $DOC.md -o $DOC.html
+echo Done
+
+DOC=inverse_laplace
+
+echo Building $DOC.slides.pdf
+pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -o $DOC.slides.pdf
+echo Building $DOC.pdf
+pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
+echo Building $DOC.html
+pandoc --mathjax -s $DOC.md -o $DOC.html
+
+
