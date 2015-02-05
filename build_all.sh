a3ca628ae2bd190c282b526bbfdbe48f294c538e
Chris Jobling <cpjobling@gmail.com>
2015-02-05 12:09:34 +0000

    correct build script

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,8 @@
 (cd introduction; ./build.sh)
 (cd week1; ./build.sh)
 (cd week2 ./build.sh)
-git sttaus
-git add -A
+git status
+git add --all .
 git commit -m "Regenerate slides and notes"
 git push
 git co master
-
