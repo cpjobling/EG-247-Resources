87ad0cb19e97295e2ba41b2356e5c58320b2919b
Chris P. Jobling <cpjobling@gmail.com>
2015-02-10 12:33:07 +0000

    Prepare week 3

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,7 @@
 (cd introduction; ./build.sh)
 (cd week1; ./build.sh)
-(cd week2 ./build.sh)
+(cd week2; ./build.sh)
+(cd week3; ./build.sh)
 git status
 git add --all .
 git commit -m "Regenerate slides and notes"
