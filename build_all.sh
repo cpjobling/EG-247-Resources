d94e6223eaca70417e8cfc53fe090072f2e75953
Chris Jobling <cpjobling@gmail.com>
2015-03-02 11:45:25 +0000

    prepare week 6 web

--- build_all.sh
+++ build_all.sh
@@ -4,6 +4,7 @@
 (cd week3; ./build.sh)
 (cd week4; ./build.sh)
 (cd week5; ./build.sh)
+(cd week6; ./build.sh)
 git status
 git add --all .
 git commit -m "Regenerate slides and notes"
---
+++ week6/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DOC=fourier2
+
+echo Building $DOC.slides.pdf
+pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -V fonttheme:professionalfonts -o $DOC.slides.pdf
+echo Building $DOC.pdf
+pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
+echo Building $DOC.html
+pandoc --mathjax -s $DOC.md -o $DOC.html
+echo Done
+
+DOC=fourier3
+
+echo Building $DOC.slides.pdf
+pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -V fonttheme:professionalfonts -o $DOC.slides.pdf
+echo Building $DOC.pdf
+pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
+echo Building $DOC.html
+pandoc --mathjax -s $DOC.md -o $DOC.html
