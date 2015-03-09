33b433c1b4c7fbe9046d271fa7646230d435a770
Chris Jobling <cpjobling@gmail.com>
2015-03-09 16:23:17 +0000

    add build scripts

--- build_all.sh
+++ build_all.sh
@@ -5,6 +5,7 @@
 (cd week4; ./build.sh)
 (cd week5; ./build.sh)
 (cd week6; ./build.sh)
+(cd week7; ./build.sh)
 git status
 git add --all .
 git commit -m "Regenerate slides and notes"
---
+++ week7/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DOC=ft1
+
+echo Building $DOC.slides.pdf
+pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -V fonttheme:professionalfonts -o $DOC.slides.pdf
+echo Building $DOC.pdf
+pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
+echo Building $DOC.html
+pandoc --mathjax -s $DOC.md -o $DOC.html
+echo Done
+
+DOC=ft2
+
+echo Building $DOC.slides.pdf
+pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -V fonttheme:professionalfonts -o $DOC.slides.pdf
+echo Building $DOC.pdf
+pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
+echo Building $DOC.html
+pandoc --mathjax -s $DOC.md -o $DOC.html
