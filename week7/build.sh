efb5300fc32bd049f39db7e8f60f8ab20ae0a671
Chris Jobling <cpjobling@gmail.com>
2015-03-09 20:24:42 +0000

    add props and tables of FT to build

--- week7/build.sh
+++ week7/build.sh
@@ -18,3 +18,7 @@ echo Building $DOC.pdf
 pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
 echo Building $DOC.html
 pandoc --mathjax -s $DOC.md -o $DOC.html
+
+# Tables of transforms and properties
+pandoc --mathjax -s ft_props.md -o ft_props.html
+pandoc --mathjax -s ft_table.md -o ft_table.html
