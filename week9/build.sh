d671af2f4105e2cb45308085504588459eeb6e9e
Chris P. Jobling <cpjobling@gmail.com>
2015-04-17 11:27:59 +0100

    add properties and transform tables builders

--- week9/build.sh
+++ week9/build.sh
@@ -18,3 +18,7 @@ echo Building $DOC.pdf
 pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
 echo Building $DOC.html
 pandoc --mathjax -s $DOC.md -o $DOC.html
+
+# Tables of transforms and properties
+pandoc --mathjax -s z_props.md -o z_props.html
+pandoc --mathjax -s z_table.md -o z_table.html
