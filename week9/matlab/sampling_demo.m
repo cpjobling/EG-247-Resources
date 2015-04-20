82af4d2d34b177eb01e8d38abbf92dabe9ef514d
Chris P. Jobling <cpjobling@gmail.com>
2015-04-20 10:53:02 +0100

    added better UI messages

--- week9/matlab/sampling_demo.m
+++ week9/matlab/sampling_demo.m
@@ -9,33 +9,35 @@
 recObj = audiorecorder;
 %% Record so
 disp('Start speaking: say "EG-247 Signals and Systems"')
-disp('Press any key when your are ready...')
+disp('Press any key when you are ready...')
 pause
 recordblocking(recObj, 5);
 disp('End of Recording.');
 % Display meta data
 recObj
 %% Play back the recording.
-disp('Press any key when your are ready...')
+disp('Press any key to hear playback ...')
 pause
 play(recObj);
 %% Store data in double-precision array.
 myRecording = getaudiodata(recObj);
 %% Plot the waveform.
+disp('Press any key to see plot of signal ...')
+pause
 plot(myRecording);
 title('Time plot of sampled sound - zoom to see details')
 xlabel('Sample number [n]')
 ylabel('Sampled sound value')
-disp('Press any key when your are ready...')
-pause
+
 %%
 % Plot the samples
+disp('Press any key to see part of the sampled data sequence ...')
+pause
 stem([1000:1049],myRecording(1000:1049))
 title('Stem plot of small sample of waveform: n = 1000 to 1049')
 xlabel('Sample number [n]')
 ylabel('Sampled sound value')
-disp('Press any key when your are ready...')
-pause
+
 %% Save the result
 Fs = recObj.SampleRate;
 audioFile = 'eg-247-message.wav';
