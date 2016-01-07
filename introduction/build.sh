975817151a8e4f4aedadea7a1ad765ee73bdde72
Chris P. Jobling <cpjobling@gmail.com>
2016-01-07 19:58:05 +0000

    regenerate introduction

--- introduction/build.sh
+++ introduction/build.sh
@@ -2,12 +2,12 @@
 
 DOC=Introduction
 
-echo Building $DOC.slides.pdf
-pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -o $DOC.slides.pdf
+echo Building $DOC.slides.html
+jupyter nbconvert $DOC.ipynb --to slides
 echo Building $DOC.pdf
-pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
-echo Building $DOC.html
-pandoc --mathjax -s $DOC.md -o $DOC.html
+jupyter nbconvert $DOC.ipynb --to pdf
+# echo Building $DOC.html
+# pandoc --mathjax -s $DOC.md -o $DOC.html
 echo Done
 
 
