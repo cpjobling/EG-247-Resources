00b21f3bf8d4bb5aae09a1d28ddf8a677ebe5012
Chris P. Jobling <cpjobling@gmail.com>
2016-01-08 15:55:02 +0000

    update website for introduction

--- introduction/build.sh
+++ introduction/build.sh
@@ -4,7 +4,7 @@ DOC=Introduction
 
 echo Building $DOC.slides.html
 jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix reveal.js
-echo "Don't forget to export PDF"
+echo "Don't forget to export PDF for Intro and Homework"
 echo Done
 
 
