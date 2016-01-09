bd7317aa002afb625909a3e96af1345d10a01b82
Chris P. Jobling <cpjobling@gmail.com>
2016-01-09 14:45:32 +0000

    modify build for jupyter

--- week1/build.sh
+++ week1/build.sh
@@ -2,13 +2,8 @@
 
 DOC=elementary_signals
 
-echo Building $DOC.slides.pdf
-pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -o $DOC.slides.pdf
-echo Building $DOC.pdf
-pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
-echo Building $DOC.html
-pandoc --mathjax -s $DOC.md -o $DOC.html
+echo Building $DOC.slides.html
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix ../introduction/reveal.js
+echo "Don't forget to export PDF for notebooks" *.ipynb
 echo Done
-
-
-
+echo Done
