671c5a48381c35935b9b49fe167c6bb7bf60e44e
Chris Jobling <cpjobling@gmail.com>
2016-02-01 07:26:14 +0000

    Reveal 2.6.2 needed

--- introduction/build.sh
+++ introduction/build.sh
@@ -3,7 +3,7 @@
 DOC=Introduction
 
 echo Building $DOC.slides.html
-jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix reveal.js
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
 echo "Don't forget to export PDF for Intro and Homework"
 echo Done
 
