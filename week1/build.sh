367ecf7a6fa505e5f357d3ef30ce6f5d2d6c433c
Chris Jobling <cpjobling@gmail.com>
2016-02-01 07:30:47 +0000

    Update reveal.js

--- week1/build.sh
+++ week1/build.sh
@@ -3,7 +3,7 @@
 DOC=elementary_signals
 
 echo Building $DOC.slides.html
-jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix ../introduction/reveal.js
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
 echo "Don't forget to export PDF for notebooks" *.ipynb
 echo Done
 echo Done
--- week2/build.sh
+++ week2/build.sh
@@ -4,13 +4,13 @@ DOC=laplace
 
 
 echo Building $DOC.slides.html
-jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix ../introduction/reveal.js
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
 
 
 DOC=inverse_laplace
 
 echo Building $DOC.slides.html
-jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix ../introduction/reveal.js
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
 
 echo "Don't forget to export PDF for notebooks" *.ipynb
 echo done
