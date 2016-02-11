2e5ef4000fb8c49731f66771ce684d767545024a
Chris P. Jobling <cpjobling@gmail.com>
2016-02-11 16:03:11 +0000

    ready to convert week 4

--- week4/build.sh
+++ week4/build.sh
@@ -3,18 +3,13 @@
 DOC=state_space
 
 echo Building $DOC.slides.pdf
-pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -V fonttheme:professionalfonts -o $DOC.slides.pdf
-echo Building $DOC.pdf
-pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
-echo Building $DOC.html
-pandoc --mathjax -s $DOC.md -o $DOC.html
-echo Done
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+
 
 DOC=convolution
 
 echo Building $DOC.slides.pdf
-pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -V fonttheme:professionalfonts -o $DOC.slides.pdf
-echo Building $DOC.pdf
-pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
-echo Building $DOC.html
-pandoc --mathjax -s $DOC.md -o $DOC.html
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+
+echo "Don't forget to export PDF for notebooks" *.ipynb
+echo done
