04bba0017a13d822efedb96ac424366a4bc12cf8
Chris P. Jobling <cpjobling@gmail.com>
2016-02-11 16:53:53 +0000

    html not pdfs being buildt

--- week4/build.sh
+++ week4/build.sh
@@ -2,13 +2,13 @@
 
 DOC=state_space
 
-echo Building $DOC.slides.pdf
+echo Building $DOC.slides.html
 jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
 
 
 DOC=convolution
 
-echo Building $DOC.slides.pdf
+echo Building $DOC.slides.html
 jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
 
 echo "Don't forget to export PDF for notebooks" *.ipynb
