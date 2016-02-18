a0dfb1298abf5df1b2cface2967b187551313339
Chris P. Jobling <cpjobling@gmail.com>
2016-02-18 15:38:38 +0000

    web pages for week 5

--- week5/build.sh
+++ week5/build.sh
@@ -2,19 +2,14 @@
 
 DOC=convolution
 
-echo Building $DOC.slides.pdf
-pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -V fonttheme:professionalfonts -o $DOC.slides.pdf
-echo Building $DOC.pdf
-pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
-echo Building $DOC.html
-pandoc --mathjax -s $DOC.md -o $DOC.html
-echo Done
+echo Building $DOC.slides.html
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+
 
 DOC=fourier1
 
-echo Building $DOC.slides.pdf
-pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -V fonttheme:professionalfonts -o $DOC.slides.pdf
-echo Building $DOC.pdf
-pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
-echo Building $DOC.html
-pandoc --mathjax -s $DOC.md -o $DOC.html
+echo Building $DOC.slides.html
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+
+echo "Don't forget to export PDF for notebooks" *.ipynb
+echo done
