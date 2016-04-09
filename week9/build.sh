0a8601f921eb5025177ced7ad6fea5c3ecbaae5c
Chris P. Jobling <cpjobling@gmail.com>
2016-04-09 12:39:51 +0100

    regenerate web site for week9

--- week9/build.sh
+++ week9/build.sh
@@ -2,22 +2,16 @@
 
 DOC=sampling
 
-echo Building $DOC.slides.pdf
-pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -V fonttheme:professionalfonts -o $DOC.slides.pdf
-echo Building $DOC.pdf
-pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
-echo Building $DOC.html
-pandoc --mathjax -s $DOC.md -o $DOC.html
-echo Done
+echo Building $DOC.slides.html
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+
 
 DOC=z-transform
 
-echo Building $DOC.slides.pdf
-pandoc --latex-engine=xelatex -t beamer $DOC.md --slide-level=2 -V fonttheme:professionalfonts -o $DOC.slides.pdf
-echo Building $DOC.pdf
-pandoc --latex-engine=xelatex $DOC.md -o $DOC.pdf
-echo Building $DOC.html
-pandoc --mathjax -s $DOC.md -o $DOC.html
+echo Building $DOC.slides.html
+jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+
+echo "Don't forget to export PDF for notebooks" *.ipynb
 
 # Tables of transforms and properties
 pandoc --mathjax -s zt_props.md -o zt_props.html
