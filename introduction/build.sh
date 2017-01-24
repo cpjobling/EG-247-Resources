9cfcd10c85bbad467ddd15de21ceba8d4c3f018a
Chris Jobling <cpjobling@gmail.com>
2017-01-24 16:52:53 +0000

    completed week 1 www update

--- introduction/build.sh
+++ introduction/build.sh
@@ -4,7 +4,7 @@ DOC=Introduction
 
 echo Building $DOC.slides.html
 jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
-echo "Don't forget to export PDF for Intro and Homework"
+echo "Don't forget to export PDF for Intro"
 echo Done
 
 
--- week1/build.sh
+++ week1/build.sh
@@ -6,4 +6,3 @@ echo Building $DOC.slides.html
 jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
 echo "Don't forget to export PDF for notebooks" *.ipynb
 echo Done
-echo Done
