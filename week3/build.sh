bdc63a75d02b80e15ab6a17ebadaf5242320cdae
Chris Jobling <cpjobling@gmail.com>
2017-02-02 18:33:09 +0000

    updated website for week 3

--- week3/build.sh
+++ week3/build.sh
@@ -9,7 +9,8 @@ jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.ne
 DOC=circuit_analysis
 
 echo Building $DOC.slides.html
-jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+cat README-slides
+# jupyter nbconvert $DOC.ipynb --to slides
 
 echo "Don't forget to export PDF for notebooks" *.ipynb
 echo done
