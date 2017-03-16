27000480be91ede4ae52639b5d68fbc0a1940608
Chris Jobling <cpjobling@gmail.com>
2017-03-16 16:16:20 +0000

    Added new PDFs

--- week6/build.sh
+++ week6/build.sh
@@ -3,13 +3,13 @@
 DOC=fourier2
 
 
-echo Building $DOC.slides.html
+#echo Building $DOC.slides.html
 jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
 
 
 DOC=fourier3
 
-echo Building $DOC.slides.html
-jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+#echo Building $DOC.slides.html
+.jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
 
 echo "Don't forget to export PDF for notebooks" *.ipynb
