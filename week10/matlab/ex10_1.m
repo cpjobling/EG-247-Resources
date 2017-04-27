9a9ad8a8e3dfab916b67954dae80775c7227afd2
Chris Jobling <cpjobling@gmail.com>
2017-04-27 16:27:41 +0100

    outline of DFT lecture complete

--- week10/matlab/ex10_1.m
+++ week10/matlab/ex10_1.m
@@ -1,7 +1,7 @@
 % Karris Example 10.1
 % To successfully run this script you will need to download the scripts dft.m and
 % idft.m and make them available on your MATLABPATH.
-xn = [1, 2, 2, 1];
-Xm = dft(xn)
+xn = [1, 2, 2, 1]
+Xm = dft(xn,4)
 %%
-xn = idft(Xm)
+xn = idft(Xm,4)
