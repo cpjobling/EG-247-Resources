895180ee19b01de92dc1ff83c9fab003b37000c6
Chris Jobling <cpjobling@gmail.com>
2018-01-24 23:05:21 +0000

    Added introduction to website

--- introduction/build.sh
+++ introduction/build.sh
@@ -3,8 +3,7 @@
 DOC=Introduction
 
 echo Building $DOC.slides.html
-cat README-slides
-# jupyter nbconvert $DOC.ipynb --to slides 
+jupyter nbconvert $DOC.ipynb --to slides
 echo "Don't forget to export PDF for Intro"
 echo Done
 
