833980ffac16e043920399f645b9e751444068f7
Chris Jobling <cpjobling@gmail.com>
2018-01-24 22:37:23 +0000

    Tuned introduction for presentation

--- introduction/matlab/sinewave.m
+++ introduction/matlab/sinewave.m
@@ -1,4 +1,4 @@
-%% Sinusoid Example from Lecture 1
+%% Sinusoid Example from Introduction
 %  Here's the code to compute and plot the sinusoidal signal
 % $3 \cos(2\pi \cdot 2t - 3\pi/4)$
 
@@ -6,7 +6,7 @@
 t = linspace(0, 1, 100);
 %% Define x
 x = 3 * cos(2*pi*2*t - 3*pi/4);
-%% Plot result
+%% Plot result and label plot
 plot(t,x)
 title('A Sinusoidal Signal')
 xlabel('Time t (s)')
