dbb4a7c61a19a27859d9d461595c6cc2664cc1a3
Chris Jobling <cpjobling@gmail.com>
2018-01-25 14:33:59 +0000

    updated build files for weeks 1 & 2

--- week1/build.sh
+++ week1/build.sh
@@ -3,7 +3,6 @@
 DOC=elementary_signals
 
 echo Building $DOC.slides.html
-# jupyter nbconvert $DOC.ipynb --to slides 
-cat README-slides
+jupyter nbconvert $DOC.ipynb --to slides
 echo "Don't forget to export PDF for notebooks" *.ipynb
 echo Done
