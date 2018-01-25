17fdcf60a3ef949d345a14d8ed70b1a1cb60eb6a
Chris Jobling <cpjobling@gmail.com>
2018-01-25 23:56:34 +0000

    Finished lab 1

--- portfolio/lab01/foo.m
+++ portfolio/lab01/foo.m
@@ -2,5 +2,6 @@ function [y,z] = foo(x)
 % [y,z] = foo(x) accepts a numerical argument x and
 % returns two arguments y and z, where y = 2*x and
 % z = (5/9)*(x-32)
-y = 2*x
-z = (5/9)*(x-32)
+y = 2*x;
+z = (5/9)*(x-32);
+end
