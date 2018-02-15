74d96831af1c84b54fddc6933cf4135538a00116
Chris Jobling <cpjobling@gmail.com>
2018-02-15 15:21:15 +0000

    website for week 5

--- week5/build.sh
+++ week5/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DOC=convolution
+DOC=exp_fs1
 
 echo Building $DOC.slides.html
 jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
 
 
-DOC=fourier1
+DOC=exp_fs2
 
 echo Building $DOC.slides.html
 jupyter nbconvert $DOC.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
