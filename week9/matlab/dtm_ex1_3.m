023f1f2c9d63be2ad470f9721b67d5064874c4ed
Chris Jobling <cpjobling@gmail.com>
2018-04-11 16:20:47 +0100

    Week 9 converted to Matlab execution

--- week9/matlab/dtm_ex1_3.m
+++ week9/matlab/dtm_ex1_3.m
@@ -1,22 +1,23 @@
-%% Example 1 Part 3 from Session on DT Models
-%
+%% Example 5 Part 3 from Session on DT Models
 % Step response of a transfer function
-%
-% Prepared for EG-247 Signals and Systems
-% by Dr Chris P. Jobling
-%
+% 
+% Prepared for EG-247 Signals and Systems by Dr Chris P. Jobling
 %% Problem
-%
-% The difference equation describing the input-output relationship of a DT system with zero initial conditions, is:
-%
+% The difference equation describing the input-output relationship of a DT system 
+% with zero initial conditions, is:
+% 
 % $$y[n] - 0.5 y[n-1] + 0.125 y[n-2] = x[n] + x[n -1]$$
 % 
 % Part 3 Compute: The DT step response
 %% Solution
 % Numerator
-Nz = [1 1 0] % $z^2 + z$
-%%
+% 
+% $$z^2+z$$
+
+Nz = [1 1 0]
+%% 
 % Denominator
+%%
 syms z
 Dz = (z^2 - 0.5*z + 0.125)*(z - 1);
 %%
@@ -24,22 +25,24 @@ collect(Dz)
 %%
 Dz = sym2poly(Dz)
 %% Poles and residues
+%%
 [r,p,k] = residue(Nz,Dz)
 %% Response
+%%
 Ts = 1 % sampling time
 z = tf('z', Ts); % define z in symbolic form
 Hz = (z^2 + z)/(z^2 - 0.5*z + 0.125)
-yn = impulse(Hz, 15)
+yn = step(Hz, 15)
 stem([0:15], yn)
 grid
-title('Example 1 - Part 3')
+title('Example 5 - Part 3')
 xlabel('n')
 ylabel('Step response y[n]')
 %% Response as stepwise continuous y(t)
+%%
 step(Hz,15)
 grid
-title('Example 1 - Part 3 - As Analogue Signal')
+title('Example 5 - Part 3 - As Analogue Signal')
 xlabel('nTs [s]')
 ylabel('Step response y(t)')
 axis([0,15,0,3.5])
-
