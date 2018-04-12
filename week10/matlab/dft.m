7b8c9365523f768d93250daa6c1cfe4504642292
Chris P. Jobling <cpjobling@cpjobling.net>
2018-04-12 17:13:19 +0100

    Upgraded to Matlab Delivery

--- week10/matlab/dft.m
+++ week10/matlab/dft.m
@@ -6,9 +6,9 @@ function [ Xm ] = dft( xn, N )
 % xn = N-point finite-duration sequence
 %  N = length of DFT
 %
-n = [0:1:N-1]          % row vector for n
-m = [0:1:N-1]          % row vector for m
-WN = exp(-j*2*pi/N)    % Wn factor
-nm = n'*m              % creates an N by N matrix of nm values
-WNnm = WN .^ nm        % DFT matrix
-Xm = xn * WNnm         % row vector of DFT coefficients
+n = [0:1:N-1];          % row vector for n
+m = [0:1:N-1];          % row vector for m
+WN = exp(-j*2*pi/N);    % Wn factor
+nm = n'*m;              % creates an N by N matrix of nm values
+WNnm = WN .^ nm;        % DFT matrix
+Xm = xn * WNnm;         % row vector of DFT coefficients
